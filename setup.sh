21d58461b70c7e7d6800dc99b3c540783d3fcf0d
sujin0529 <sujin.jang@prosys.kr>
2022-08-04 14:06:46 +0900

    [Init] initialize

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+export OPAMYES=1
+
+NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
+OCAML_VERSION="4.13.1"
+UNITGEN_OPAM_SWITCH=unitgen-"$OCAML_VERSION"
+opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup
+
+switch_exists=no
+for installed_switch in $(opam switch list --short); do
+  if [[ "$installed_switch" == "$UNITGEN_OPAM_SWITCH" ]]; then
+    switch_exists=yes
+    break
+  fi
+done
+
+if [ "$switch_exists" = "no" ]; then
+  opam switch create $UNITGEN_OPAM_SWITCH $OCAML_VERSION
+else
+  opam switch $UNITGEN_OPAM_SWITCH
+fi
+
+eval $(SHELL=bash opam config env --switch=$UNITGEN_OPAM_SWITCH)
+opam install -j $NCPU yojson
+make
