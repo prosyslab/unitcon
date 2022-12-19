1ba2f9d775c142eab94c08b6543d8e8d49db4836
sujin0529 <sujin.jang@prosys.kr>
2022-12-19 15:40:51 +0900

    [Test] fix simple example execution script

--- run.sh
+++ run.sh
@@ -2,16 +2,20 @@
 
 INFER_BIN=~/unittest-infer/infer/bin/infer
 cd test
+mkdir source
 $INFER_BIN capture -- javac Main.java
 $INFER_BIN analyze --biabduction-only
-$INFER_BIN debug --procedures --procedures-summary-json > infer-out/summary.json
+cp infer-out/report.json source/report.json
+cp infer-out/error_summarys source/error_summarys
+$INFER_BIN analyze --biabduction-only --find-missing-summary
+$INFER_BIN debug --procedures --procedures-summary-json > source/summary.json
 cd ..
 
-TRACE=test/infer-out/report.json
-ERROR_SUMMARY=test/infer-out/error_summarys
-SUMMARY=test/infer-out/summary.json
+TRACE=test/source/report.json
+ERROR_SUMMARY=test/source/error_summarys
+SUMMARY=test/source/summary.json
 
 python3 error_summary_parser.py $ERROR_SUMMARY
 ERROR_SUMMARY=$ERROR_SUMMARY.json
 
-./unitgen $TRACE $ERROR_SUMMARY $SUMMARY
+./unitgen $SUMMARY $ERROR_SUMMARY $TRACE
