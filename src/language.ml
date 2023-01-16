e50270f075ea0c59f6aeb042895cce49b7516b5c
sujin0529 <sujin.jang@prosys.kr>
2023-01-16 17:56:52 +0900

    [Language] reconstruct language.ml

--- src/language.ml
+++ src/language.ml
@@ -1,73 +1,134 @@
 module Json = Yojson.Safe
 module JsonUtil = Yojson.Safe.Util
 
-type var = string
+type method_name = string
+
+type class_name = string
+
+type file_name = string
 
 type modifier = Public | Private | Protected | Default [@@deriving compare]
 
+type typ = Int | Float | String | Object of class_name | None
+
+type id = string (*e.g. i *)
+
+type variable = This of typ | Var of typ * id
+
+type params = variable list
+
+type symbol = string (*e.g. v1 *)
+
 let modifier_of_json json =
   JsonUtil.to_string json |> function
-  | "public" -> Public
-  | "private" -> Private
-  | "protected" -> Protected
-  | "default" -> Default
+  | "Public" -> Public
+  | "Private" -> Private
+  | "Protected" -> Protected
+  | "Default" -> Default
   | s -> failwith ("Unknown modifier " ^ s)
 
-let param_of_json json = List.map JsonUtil.to_string json
+let params_of_json json = List.map JsonUtil.to_string json
 
-type args = var list
-
-type exp =
-  | Command of string * args
-  | String of string
-  | Int of int
-  | New of string * args
-
-type t = Assignment of var * exp | Statement of var * exp | Sequence of t * t
-
-module Method = struct
-  let compare_string = String.compare
-
-  let compare_list = List.compare
-
-  type t = { name : string; modifier : modifier; param : string list }
-  [@@deriving compare]
-
-  let hash = Hashtbl.hash
-
-  let equal = ( = )
-
-  let from_json json =
-    let name = Yojson.Safe.Util.member "name" json |> JsonUtil.to_string in
-    let modifier =
-      Yojson.Safe.Util.member "modifier" json |> modifier_of_json
-    in
-    let param =
-      Yojson.Safe.Util.member "param" json
-      |> Yojson.Safe.Util.to_list |> param_of_json
-    in
-    { name; modifier; param }
-end
-
-module MethodMap = struct
+module MethodInfo = struct
   module M = Map.Make (struct
-    type t = string
+    type t = method_name
 
     let compare = compare
   end)
 
-  type t = Method.t M.t
+  type info = {
+    modifier : modifier;
+    formal_params : params;
+    filename : file_name;
+  }
 
-  let of_json json =
-    Yojson.Safe.Util.to_list json
-    |> List.fold_left
-         (fun mmap item ->
-           let m = Method.from_json item in
-           M.add m.Method.name m mmap)
-         M.empty
+  type t = info M.t
 end
 
-let default typ =
-  Method.{ name = typ ^ ".<init>()"; modifier = Public; param = [ "" ] }
+module Relation = struct
+  module M = Map.Make (struct
+    type t = symbol
 
-let null = Method.{ name = "null"; modifier = Public; param = [ "" ] }
+    let compare = compare
+  end)
+
+  type t = symbol M.t
+end
+
+module Value = struct
+  type value =
+    | Int of int
+    | Float of float
+    | String of string
+    | PlusInf
+    | MinusInf
+    | Null
+
+  type t =
+    | Eq of symbol * value
+    | Neq of symbol * value
+    | Le of symbol * value
+    | Lt of symbol * value
+    | Ge of symbol * value
+    | Gt of symbol * value
+    | Between of symbol * value * value
+    | Outside of symbol * value * value
+
+  let is_le str = String.contains str '<' && String.contains str '='
+
+  let is_lt str = String.contains str '<' && String.contains str '=' |> not
+
+  let is_ge str = String.contains str '>' && String.contains str '='
+
+  let is_gt str = String.contains str '>' && String.contains str '=' |> not
+
+  let is_neq str = String.contains str '!' && String.contains str '='
+
+  let is_eq str =
+    String.contains str '='
+    && String.contains str '!' |> not
+    && String.contains str '>' |> not
+    && String.contains str '<' |> not
+
+  let is_between str =
+    String.contains str 'i' && String.contains str 'n'
+    && String.contains str '['
+    && String.contains str 't' |> not
+
+  let is_outside str =
+    String.contains str 'n' && String.contains str 'o'
+    && String.contains str 't' && String.contains str '['
+end
+
+module Condition = struct
+  module M = Map.Make (struct
+    type t = symbol
+
+    let compare = compare
+  end)
+
+  type rh = RH_Var of id | RH_Symbol of symbol
+
+  type var = rh M.t
+
+  type mem = rh list M.t
+
+  type t = var * mem
+end
+
+type summary = {
+  relation : Relation.t;
+  value : Value.t list;
+  precond : Condition.t;
+  postcond : Condition.t;
+}
+
+module SummaryMap = struct
+  module M = Map.Make (struct
+    type t = method_name
+
+    let compare = compare
+  end)
+
+  type t = summary list M.t
+end
