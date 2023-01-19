1cbd89299bd82e3532712388bc7e442e3635fb18
sujin0529 <sujin.jang@prosys.kr>
2023-01-19 15:06:40 +0900

    [Callgraph] fix parsing error

--- src/callgraph.ml
+++ src/callgraph.ml
@@ -25,17 +25,37 @@ module G = struct
   let edge_attributes _ = []
 end
 
+let get_caller_method_name assoc =
+  let split_name name =
+    if String.contains name ' ' then
+      name |> String.split_on_char ' ' |> List.tl |> List.hd
+    else name
+  in
+  let method_name =
+    JsonUtil.member "method" assoc
+    |> JsonUtil.to_list |> List.hd |> JsonUtil.to_string |> split_name
+  in
+  method_name
+
+let get_callee_method_name assoc =
+  let split_name name =
+    if String.contains name ' ' then
+      name |> String.split_on_char ' ' |> List.tl |> List.hd
+    else name
+  in
+  let method_name =
+    JsonUtil.member "callee" assoc
+    |> JsonUtil.to_list
+    |> List.map (fun callee -> callee |> JsonUtil.to_string |> split_name)
+  in
+  method_name
+
 let of_json json =
   JsonUtil.to_list json
   |> List.fold_left
        (fun g element ->
-         let caller =
-           JsonUtil.member "method" element
-           |> JsonUtil.to_list |> List.hd |> JsonUtil.to_string
-         in
-         let callees = JsonUtil.member "callee" element |> JsonUtil.to_list in
-         callees
-         |> List.map JsonUtil.to_string
+         let caller = get_caller_method_name element in
+         get_callee_method_name element
          |> List.fold_left (fun g callee -> G.add_edge g callee caller) g)
        G.empty
 
