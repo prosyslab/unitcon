cfcc48de84321a26063e0f3cf9ddd160134e5087
sujin0529 <sujin.jang@prosys.kr>
2023-08-01 16:52:06 +0900

    [Modeling] modify type of string and add array

--- src/modeling.ml
+++ src/modeling.ml
@@ -288,6 +288,35 @@ let string_postmem =
   |> Condition.M.add (Condition.RH_Symbol "v5")
        (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v4") value_map)
 
+let array_var =
+  Condition.M.empty
+  |> Condition.M.add (Condition.RH_Symbol "v2") (Condition.RH_Var "size")
+  |> Condition.M.add (Condition.RH_Symbol "v1") (Condition.RH_Var "this")
+
+let array_premem =
+  let value_map = Condition.M.empty in
+  Condition.M.empty
+  |> Condition.M.add (Condition.RH_Symbol "v1")
+       (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v3") value_map)
+  |> Condition.M.add (Condition.RH_Symbol "v2")
+       (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v4") value_map)
+  |> Condition.M.add (Condition.RH_Symbol "v3")
+       (value_map
+       |> Condition.M.add (Condition.RH_Var "size") (Condition.RH_Symbol "v5"))
+
+let array_postmem =
+  let value_map = Condition.M.empty in
+  Condition.M.empty
+  |> Condition.M.add (Condition.RH_Symbol "v1")
+       (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v3") value_map)
+  |> Condition.M.add (Condition.RH_Symbol "v2")
+       (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v4") value_map)
+  |> Condition.M.add (Condition.RH_Symbol "v3")
+       (value_map
+       |> Condition.M.add (Condition.RH_Var "size") (Condition.RH_Symbol "v5"))
+  |> Condition.M.add (Condition.RH_Symbol "v5")
+       (Condition.M.add Condition.RH_Any (Condition.RH_Symbol "v4") value_map)
+
 let hashmap_put_summary =
   Language.
     {
@@ -395,6 +424,16 @@ let string_summary =
       value = Value.M.empty;
       precond = (string_var, string_premem);
       postcond = (string_var, string_postmem);
+      args = [];
+    }
+
+let array_summary =
+  Language.
+    {
+      relation = Relation.M.empty;
+      value = Value.M.empty;
+      precond = (array_var, array_premem);
+      postcond = (array_var, array_postmem);
       args = [];
     }
 
@@ -525,7 +564,7 @@ let obj_info =
 
 let string_info =
   let this = ("java.lang.String", Language.This (Object "String")) in
-  let arg = ("java.lang.String", Language.Var (Object "String", "s")) in
+  let arg = ("java.lang.String", Language.Var (String, "con_string")) in
   MethodInfo.
     {
       modifier = Language.Public;
@@ -534,6 +573,104 @@ let string_info =
       return = "";
       filename = "";
     }
+
+let int_array_info =
+  let this = ("", Language.This (Array Int)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let long_array_info =
+  let this = ("", Language.This (Array Long)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let float_array_info =
+  let this = ("", Language.This (Array Float)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let double_array_info =
+  let this = ("", Language.This (Array Double)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let bool_array_info =
+  let this = ("", Language.This (Array Bool)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let char_array_info =
+  let this = ("", Language.This (Array Char)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let string_array_info =
+  let this = ("", Language.This (Array String)) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+let object_array_info =
+  let this = ("", Language.This (Array (Object "Object"))) in
+  let arg = ("", Language.Var (Int, "con_size")) in
+  MethodInfo.
+    {
+      modifier = Language.Public;
+      is_static = false;
+      formal_params = [ this; arg ];
+      return = "";
+      filename = "";
+    }
+
+(*TODO: array_array_info *)
 
 let add_java_package_summary mmap =
   SummaryMap.M.add "HashMap.put(Object,Object)" [ hashmap_put_summary ] mmap
@@ -547,6 +684,14 @@ let add_java_package_summary mmap =
   |> SummaryMap.M.add "FileInputStream.<init>(File)" [ file_input_summary ]
   |> SummaryMap.M.add "Object.<init>()" [ obj_summary ]
   |> SummaryMap.M.add "String.<init>(String)" [ string_summary ]
+  |> SummaryMap.M.add "IntArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "LongArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "FloatArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "DoubleArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "BoolArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "CharArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "StringArray.<init>(int)" [ array_summary ]
+  |> SummaryMap.M.add "ObjectArray.<init>(int)" [ array_summary ]
 
 let add_java_package_method mmap =
   MethodInfo.M.add "HashMap.put(Object,Object)" hashmap_put_info mmap
@@ -560,6 +705,14 @@ let add_java_package_method mmap =
   |> MethodInfo.M.add "FileInputStream.<init>(File)" file_input_info
   |> MethodInfo.M.add "Object.<init>()" obj_info
   |> MethodInfo.M.add "String.<init>(String)" string_info
+  |> MethodInfo.M.add "IntArray.<init>(int)" int_array_info
+  |> MethodInfo.M.add "LongArray.<init>(int)" long_array_info
+  |> MethodInfo.M.add "FloatArray.<init>(int)" float_array_info
+  |> MethodInfo.M.add "DoubleArray.<init>(int)" double_array_info
+  |> MethodInfo.M.add "BoolArray.<init>(int)" bool_array_info
+  |> MethodInfo.M.add "CharArray.<init>(int)" char_array_info
+  |> MethodInfo.M.add "StringArray.<init>(int)" string_array_info
+  |> MethodInfo.M.add "ObjectArray.<init>(int)" object_array_info
 
 let add_java_package_inheritance ig =
   let add_inheritance super child ig = IG.add_edge ig super child in
