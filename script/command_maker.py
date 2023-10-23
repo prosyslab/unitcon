143241cb5e6f67477050bb9be44cf41351d9fc03
sujin0529 <sujin.jang@prosys.kr>
2023-10-23 13:40:33 +0900

    [Script] add command execution for dependency extraction

--- script/command_maker.py
+++ script/command_maker.py
@@ -4,33 +4,38 @@ import os
 import shutil
 import subprocess
 
-manifest_file = "Manifest"
+manifest_file_name = "Manifest"
 dependency_jar = "with_dependency.jar"
 test_file_name = "UnitconTest"
 
-def make_dependency_jar(project_dir, classpaths):
-  current_dir = os.getcwd()
-  os.chdir(project_dir)
-  contents = 'Class-Path: ' +'\n  '.join(classpaths) + '\nMain-Class: ' + test_file_name
-  with open(manifest_file, 'w') as f:
-    f.write(contents)
-    f.write('\n')
 
-  command = ['jar', '-cmf', manifest_file, dependency_jar, '$(find . -name "*.jar")', '$(find . -name "classes")']
-  os.system(' '.join(command)) # make dependency_jar file
+def make_dependency_jar(project_dir, classpaths):
+    manifest_file = os.path.join(project_dir, manifest_file_name)
+    contents = 'Class-Path: ' + '\n  '.join(
+        classpaths) + '\nMain-Class: ' + test_file_name
+    with open(manifest_file, 'w') as f:
+        f.write(contents)
+        f.write('\n')
+
+    command = [
+        'jar', '-cmf', manifest_file_name, dependency_jar,
+        '$(find . -name "*.jar")', '$(find . -name "classes")'
+    ]
+    subprocess.run(' '.join(command), cwd=project_dir,
+                   shell=True)  # make dependency_jar file
 
 
 def write_command(cmd_type, file):
-  command = []
-  if cmd_type == "build":
-    command = ["javac", "-cp", dependency_jar, test_file_name + ".java"]
-  elif cmd_type == "test":
-    command = ["java", "-cp", dependency_jar + ":.", test_file_name]
-  assert len(command) > 0, "Failed to make command line"
-  
-  with open(file, 'w') as f:
-    f.write(' '.join(command))
-    f.write('\n')
+    command = []
+    if cmd_type == "compile":
+        command = ["javac", "-cp", dependency_jar, test_file_name + ".java"]
+    elif cmd_type == "test":
+        command = ["java", "-cp", dependency_jar + ":.", test_file_name]
+    assert len(command) > 0, "Failed to make command line"
+
+    with open(file, 'w') as f:
+        f.write(' '.join(command))
+        f.write('\n')
 
 
 def collect_classpaths(project_dir):
@@ -54,19 +59,33 @@ def make_build_command(project_dir):
     make_dependency_jar(project_dir, classpaths)
 
     file_path = os.path.join(project_dir, "unitcon_properties")
-    build_file = os.path.join(file_path, "build_command")
+    build_file = os.path.join(file_path, "compile_command")
     test_file = os.path.join(file_path, "test_command")
-    write_command("build", build_file)
+    write_command("compile", build_file)
     write_command("test", test_file)
 
 
+def execute_build_cmd(project_dir):
+    build_cmd_file = os.path.join(project_dir, "unitcon_properties",
+                                  "unitcon_build_command")
+    assert os.path.isfile(build_cmd_file), f"Failed to build {project_dir}"
+    with open(build_cmd_file, "r") as f:
+        subprocess.run([cmd for cmd in f.readlines()],
+                       cwd=project_dir,
+                       shell=True)
+
+
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("project", type=pathlib.Path, default=None,
-                      help='Project directory where need to create build command files')
-  args = parser.parse_args()
-  make_build_command(args.project)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "project",
+        type=pathlib.Path,
+        default=None,
+        help='Project directory where need to create build command files')
+    args = parser.parse_args()
+    execute_build_cmd(args.project)
+    make_build_command(args.project)
 
 
 if __name__ == "__main__":
-  main()
+    main()
