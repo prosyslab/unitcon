7e8c7d7156725e18503b590f6524b7420af2ecdd
pingpingy1 <pingpingy@kaist.ac.kr>
2024-02-21 14:19:32 +0900

    [Script] modify filter_result

--- script/filter_result.py
+++ script/filter_result.py
@@ -2,54 +2,65 @@
 
 import argparse
 import os
+import shutil
 
 
-def remove_empty_tcs(result_dir: str) -> None:
+def remove_empty_dirs(project_dir: str) -> None:
     """Removes empty testcases, both due to timeout or queue exhaustion.
 
-    :param result_dir: Path to the result directory created by Unitcon
+    :param project_dir: Main directory path of the target project
     """
-    if not os.path.isdir(result_dir):
-        raise FileNotFoundError(f"Directory not found: {result_dir}")
+    all_result_dirs: list[str] = [
+        result_dir
+        for result_dir in os.listdir(project_dir)
+        if os.path.isdir(os.path.join(project_dir, result_dir))
+        and result_dir.startswith("results_")
+    ]
+    exclude_files: set[str] = {
+        "log.txt",
+        "UnitconEnum.java",
+        "UnitconEnum.class",
+        "UnitconInterface.java",
+        "UnitconInterface.class",
+    }
 
-    total_cnt: int = 0
     empty_cnt: int = 0
+    tc_cnt: int = 0
+    for result_dir in all_result_dirs:
+        is_empty: bool = True
+        full_path: str = os.path.join(project_dir, result_dir)
+        all_files = os.listdir(full_path)
 
-    for filename in os.listdir(result_dir):
-        if not (
-            len(filename) > 7 and filename[:7] == "result-" and filename[7:].isdigit()
-        ):
-            continue
-        remove: bool = False
-        total_cnt += 1
-        with open(os.path.join(result_dir, filename), "r", encoding="utf-8") as f:
-            lines: list[str] = f.readlines()
+        for file in all_files:
+            if not file in exclude_files:
+                is_empty = False
+                break
 
-            if len(lines) < 2 or lines[1].strip() == "time-out":
-                remove = True
-                empty_cnt += 1
-                print(f"Removing {filename}")
-
-        if remove:
-            os.remove(os.path.join(result_dir, filename))
+        if is_empty:
+            empty_cnt += 1
+            print(f"Empty results: {result_dir}")
+            shutil.rmtree(full_path)
+        else:
+            tc_cnt += len(set(all_files) - exclude_files) // 2
 
     print("\n=================================================\n")
-    print(f"Total files: {total_cnt}")
-    print(f"Empty files: {empty_cnt}")
-    print(f"Remaining:   {total_cnt - empty_cnt}")
+    print(f"Total dirs: {len(all_result_dirs)}")
+    print(f"Empty dirs: {empty_cnt}")
+    print(f"Remaining : {len(all_result_dirs) - empty_cnt}")
+    print(f"Testcases : {tc_cnt}")
 
 
 def main() -> None:
     """Removes empty testcases found in the provided result directory."""
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
     parser.add_argument(
-        "result_dir",
+        "project_dir",
         type=str,
         default=None,
-        help="Path to the result directory created by Unitcon",
+        help="Path to the project directory analyzed by Unitcon",
     )
     args: argparse.Namespace = parser.parse_args()
-    remove_empty_tcs(args.result_dir)
+    remove_empty_dirs(args.project_dir)
 
 
 if __name__ == "__main__":
